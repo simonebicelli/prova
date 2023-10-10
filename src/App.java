b1e411ed4c7359ddf1f0cd25cd5a74d5f21a8b3f
simonebicelli <bicelli.simone23@gmail.com>
2023-10-10 11:26:17 +0200

    commit tre

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        cavagliere prova = new cavagliere("f", 0, 0, false);
-       System.out.println(prova.gatname());
+        
     }
 }
